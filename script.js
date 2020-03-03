0c04fc051d25de769725876a66840b6b9efe7a8d
cheebachong <atrodondi@gmail.com>
2020-03-02 16:29:34 -0800

    made changes to allow user to add more gifs to page and still be able to click them on/off as far as animation. working on hw bonus parts check on #2

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// working on BONUS Features:
+// 1. Ensure your app is fully mobile responsive. DONE!! with bootstrap
+
+// 2.Allow users to request additional gifs to be added to the page.
+// Each request should ADD 10 gifs to the page, NOT overwrite the existing gifs.
+// got it done, now it adds 10 more gifs to teh page, and i can animate/stop the gifs/stills on click. i still think there is a better/more efficient way of doing it.
+
+//3. List additional metadata (title, tags, etc) for each gif in a clean and readable format.
+
+//4. Integrate this search with additional APIs such as OMDB, or Bands in Town. Be creative and build something you are proud to showcase in your portfolio
+
+// 5.Allow users to add their favorite gifs to a favorites section.
+
+// This should persist even when they select or add a new topic.
+// If you are looking for a major challenge, look into making this section persist even when the page is reloaded(via localStorage or cookies). need to fix giving the ID to each image so we can ADD to the div, 10, 20, 30, not replace each call with 10 new gifs. we can make the change fast, but then the function to animate each iamge breaks because we are using duplicate ID's. need to change the code.
+
 var items = [
   "dog",
   "cat",
@@ -22,7 +38,8 @@ var items = [
   "dolphin",
   "mountain"
 ];
-var gifObj;
+var gifs = [];
+var idCount = 0;
 //function that populates the button div with different buttons from the item array
 function makeButton() {
   $("#buttonDiv").empty();
@@ -49,19 +66,23 @@ function buttonClick() {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      $("#gifDiv").empty();
-      for (var i = 0; i < response.data.length; i++) {
-        $("#gifDiv").append(
-          " <span class=' m-1 float-left'> <p> Rating: " +
-            response.data[i].rating +
-            "</p>  <img  id ='" +
-            i +
+      console.log(response.data);
+      for (let i = 0; i < response.data.length; i++) {
+        $("#gifDiv").prepend(
+          " <span class=' m-1 d-inline-block'>  <img  id ='" +
+            idCount +
             "'class= 'img img-fluid m-1'src='" +
             response.data[i].images.fixed_height_still.url +
-            "'></span>"
+            "'> <p> Rating: " +
+            response.data[i].rating +
+            "</p> </span>"
         );
+        idCount++;
       }
-      gifObj = response.data;
+      for (let i = 0; i < response.data.length; i++) {
+        gifs.push(response.data[i]);
+      }
+      console.log(gifs);
     });
   });
 }
@@ -69,7 +90,9 @@ function buttonClick() {
 function imageClick() {
   $("#gifDiv").on("click", "img", function() {
     var clickID = event.target.id;
-    var clicked = gifObj[clickID];
+    console.log(event.target.src);
+    var clicked = gifs[clickID];
+    console.log(clicked);
     if ($("#" + clickID).attr("src") == clicked.images.fixed_height_still.url) {
       $("#" + clickID).replaceWith(
         "<img id ='" +
@@ -89,7 +112,7 @@ function imageClick() {
     }
   });
 }
-//function that controls what happens when the user creates a new item button
+//function that controls what happens when the user creates a new item button.
 function makeGif() {
   $("#findGif").on("click", function() {
     var Gif = $("#gifInput").val();
